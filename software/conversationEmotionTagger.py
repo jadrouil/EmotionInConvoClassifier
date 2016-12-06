295151e71a791b37962414405d3e0811dba68086
Jerm <jadrouil@umich.edu>
2016-12-05 19:44:23 -0500

    created stub functions

---
+++ software/conversationEmotionTagger.py
@@ -0,0 +1,16 @@
+
+
+class conversationEmotionTagger:
+	def __init__(self):
+		'''takes a conversation as input and outputs a tagged conversation'''
+
+	def testAndCompare(self,convo):
+		'''takes a convo, makes predicitions, and records accuracy
+		returns a labeled conversation'''
+
+	def accuracy(self):
+		'''returns accuracy of system over all test instances'''
+	def test(self, convo):
+		'''takes a convo and returns a labeled predicition
+		does not require an eTagged input'''
+
---
+++ software/message.py
@@ -0,0 +1,11 @@
+
+#a conversation will just be a list of messages
+class message:
+	'''container for data that will be used by algorithm to label emotion'''
+	def __init__(self, content, eTag, userId):
+		self._content = content
+		self._eTag = tag
+		self._userId = userId
+
+
+
