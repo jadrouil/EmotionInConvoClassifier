5249ab797d2d92e31ba632943b9b729ae47ee078
QuincyD <quincyd@umich.edu>
2016-12-10 15:27:08 -0500

    initialize

--- software/ViterbiTable.py
+++ software/ViterbiTable.py
@@ -19,11 +19,24 @@ class ViterbiTable:
 			pEmotionRecv = ecc.pGivenReceived(self._emotions[i], "START")
 			self._table[0].addRowToCell(pEmotion + pEmotionSent + pEmotionRecv, -1)
 
-		user2msgindex = 1
+		user2msgIndex = 1
 		for i in range(0, len(self._emotions)):
 			inst = instance(None, self._convo[1].content)
 			pEmotion = emc.test(inst, self._emotions[i])
 			pEmotionSent = ecc.pGivenSent(self._emotions[i], "START")
-			pEmotionRecv = self._maximizingRecvP(ecc)
+			pEmotionRecv = self._maximizingRecvP(ecc, user1msgIndex)
 			self._table[0].addRowToCell(pEmotion + pEmotionSent + pEmotionRecv, -1)
-		return user1msgIndex,user2msgindex
+		return user1msgIndex,user2msgIndex
+
+
+	def iterate(self, user1msgIndex,user2msgIndex, ecc, emc):
+		for i in range(2, len(self._convo)):
+			newCell = ViterbiCell()
+			if self._convo[i].user() == 1:
+				self._fillCell(newCell, user1msgIndex, user2msgIndex)
+				user1msgIndex = i
+			else:
+				self._fillCell(newCell, user2msgIndex, user1msgIndex)
+				user2msgIndex = i
+			self._table.append(newCell)
+				
