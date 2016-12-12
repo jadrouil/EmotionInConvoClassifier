254554ad3ab8320ee1605f17041b2b71fe0d4a41
Jerm <jadrouil@umich.edu>
2016-12-12 15:57:03 -0500

    whole algorithm appears to work with no training

--- software/tests/testConvoAndBayes.py
+++ software/tests/testConvoAndBayes.py
@@ -2,6 +2,10 @@ import sys
 sys.path.insert(0, '../')
 
 from taggerFactory import *
+from message import *
 
 
-createConversationEmotionTagger("emotions", "conversations", "bayes", "hotwords")
+tagger = createConversationEmotionTagger("emotions", "conversations", "bayes", "hotwords")
+convo = [message("blah blah blah", None, 1), message("wah wah wah", None, 2), message("bbb", None, 1), message("ccc", None, 2)]
+
+print tagger.test(convo)
