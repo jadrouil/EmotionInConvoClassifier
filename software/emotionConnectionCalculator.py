1c59ecaebc3e28254859a75ec5ecb11607dccaf3
Jerm <jadrouil@umich.edu>
2016-12-13 10:29:52 -0500

    calc probability for p(e | prev) changed to reflect likelihood emotions

--- software/emotionConnectionCalculator.py
+++ software/emotionConnectionCalculator.py
@@ -9,16 +9,25 @@ class emotionConnectionCalculator:
 		self._countRecvEmotion = defaultdict(lambda: defaultdict(lambda: 0.0))
 		self._totalSentEmotion = defaultdict(lambda: 0.0)
 		self._totalRecvEmotion = defaultdict(lambda: 0.0)
+		self._totalEmotions = 0.0
+		self._emotionCount = defaultdict(lambda: 0.0)
 
 	def trainSent(self, emotion, prevSentEmotion):
 		self._countSentEmotion[prevSentEmotion][emotion] += 1.0
 		self._totalSentEmotion[prevSentEmotion] += 1.0
+		self._emotionCount[emotion] += 1.0
+		self._totalEmotions += 1.0
 
 	def trainReceive(self, emotion, prevRecvEmotion):
 		self._countRecvEmotion[prevRecvEmotion][emotion] += 1.0
-		self._totalRecvEmotion[prevRecvEmotion] += 1.0
+		self._totalRecvEmotion[prevRecvEmotion] += 1.
+		self._emotionCount[emotion] += 1.0
+		self._totalEmotions += 1.0
+
 
 	def pGivenSent(self, emotion, prevSentEmotion):
-		return math.log((self._countSentEmotion[prevSentEmotion][emotion] + 1.0) / (self._totalSentEmotion[prevSentEmotion] + len(self._countSentEmotion[prevSentEmotion])))
+		pEmotion = self._emotionCount[emotion] / self._totalEmotions
+		return math.log((self._countSentEmotion[prevSentEmotion][emotion] + 1.0) / (self._totalSentEmotion[prevSentEmotion] + len(self._countSentEmotion[prevSentEmotion])) * pEmotion)
 	def pGivenReceived(self, emotion, prevRecvEmotion):
-		return math.log((self._countRecvEmotion[prevRecvEmotion][emotion]  + 1.0)/ (self._totalRecvEmotion[prevRecvEmotion] + len(self._countRecvEmotion[prevRecvEmotion]) ))
+		pEmotion = self._emotionCount[emotion] / self._totalEmotions
+		return math.log((self._countRecvEmotion[prevRecvEmotion][emotion]  + 1.0)/ (self._totalRecvEmotion[prevRecvEmotion] + len(self._countRecvEmotion[prevRecvEmotion]) ) * pEmotion)
