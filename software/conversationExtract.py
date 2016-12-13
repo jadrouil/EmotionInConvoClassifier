179c964bafd238bdf45e571e5cb31f862d49aba3
Matthew Terwilliger <matt@terwilligers.com>
2016-12-12 22:31:11 -0500

    Add conversation extractor

---
+++ software/conversationExtract.py
@@ -0,0 +1,45 @@
+import json
+
+from message import message
+
+def conversationExtract(conversationTrainfile, emotions):
+    '''Extract conversations from a JSON file 
+
+    Return: [[msg, ...], ...] nested list of conversations
+    '''
+    with open(conversationTrainfile) as trainfile:
+        trainfile = json.loads(trainfile.read())
+
+        conversations = []
+        for conversation in trainfile:
+            messages = []
+
+            # default to B since A needs to come first
+            lastUid = 2
+            for raw_message in conversation['conversation']:
+                if raw_message['tag'] not in emotions:
+                    raise RuntimeError('Unseen emotion')
+
+                if raw_message['user'] == 'A':
+                    uid = 1
+                elif raw_message['user'] == 'B':
+                    uid = 2
+                else:
+                    raise RuntimeError('Bad uid')
+
+                if lastUid == 2:
+                    # next user should be 1
+                    assert(uid == 1)
+                else:
+                    # next user should be 2
+                    assert(uid == 2)
+
+                lastUid = uid
+
+                messages.append(message(
+                  content=raw_message['message'],
+                  eTag=raw_message['tag'],
+                  userId=uid
+                ))
+
+            conversations.append(messages)
