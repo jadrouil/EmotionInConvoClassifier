a65edbc3a51f4cf68706624edf4a2d800191110c
Matthew Terwilliger <matt@terwilligers.com>
2016-12-12 19:16:38 -0500

    One conversation at a time

--- datasets/manualTagHelper.py
+++ datasets/manualTagHelper.py
@@ -7,67 +7,63 @@ import json
 with open(sys.argv[1]) as f:
     raw_conversations = f.read().split('\n\n')
 
-with open(sys.argv[3]) as f:
+with open(sys.argv[2]) as f:
     emotions = f.read().split('\n')
     emotions = [x.lower() for x in emotions if len(x) > 0]
 
-start_index = input('There are {} conversations. Start index: '\
-        .format(len(raw_conversations)))
+idx = int(sys.argv[3])
+if idx < 0 or idx >= len(raw_conversations):
+    raise RuntimeError()
 
-stop_index = input('There are {} conversations. Stop index: '\
-        .format(len(raw_conversations)))
+raw_messages = raw_conversations[idx].split('\n')
+with open('convo-{}.json'.format(idx), 'w') as outfile:
+    counter = 0
+    messages = []
+    j = 0
+    while j < len(raw_messages):
+        raw_message = raw_messages[j]
 
-with open(sys.argv[2], 'w') as outfile:
-    conversations = []
-    for i in range(start_index, stop_index):
-        raw_messages = raw_conversations[i].split('\n')
-        counter = 0
-        messages = []
-        j = 0
-        while j < len(raw_messages):
-            raw_message = raw_messages[j]
+        if len(raw_message) <= 0:
+            continue #garbage
 
-            if len(raw_message) <= 0:
-                continue #garbage
+        print ''
+        print raw_message
 
-            print ''
-            print raw_message
+        user, message    = raw_message.split(': ')
+        user, message_id = user.split('.')
 
-            user, message    = raw_message.split(': ')
-            user, message_id = user.split('.')
+        tag = None
+        while True:
+            ecounter = 0
+            for e in emotions:
+                print '{}. {}'.format(ecounter, e)
+                ecounter += 1
 
-            tag = None
-            while True:
-                ecounter = 0
-                for e in emotions:
-                    print '{}. {}'.format(ecounter, e)
-                    ecounter += 1
+            tag = raw_input('Select: ')
 
-                tag = raw_input('Select: ')
+            try:
+                tag = int(tag)
+                if tag < ecounter and tag >= 0:
+                    tag = emotions[tag]
 
-                try:
-                    tag = int(tag)
-                    if tag < ecounter and tag >= 0:
-                        tag = emotions[tag]
+                    messages.append({
+                      'user': user,
+                      'message_id': message_id,
+                      'message': message,
+                      'tag': tag,
+                    })
 
-                        messages.append({
-                          'user': user,
-                          'message_id': message_id,
-                          'message': message,
-                          'tag': tag,
-                        })
+                    j += 1
 
-                        j += 1
-
-                        break
-                except ValueError:
-                    pass
-                except TypeError:
-                    pass
-
-                if tag == 'b':
-                    j = j - 1
-                    del messages[-1]
                     break
-        conversations.append(messages)
-    outfile.write(json.dumps(conversations, indent=2))
+            except ValueError:
+                pass
+            except TypeError:
+                pass
+
+            if tag == 'b':
+                j = j - 1
+                del messages[-1]
+                break
+
+    outfile.write(json.dumps(messages, indent=2))
