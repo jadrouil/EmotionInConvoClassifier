d8d87c6f9ff5455b6547c13d41a73f4e1ecc3ec9
Jerm <jadrouil@umich.edu>
2016-12-13 00:26:26 -0500

    standard casing and punc

--- software/message.py
+++ software/message.py
@@ -1,9 +1,11 @@
-
+import string
 #a conversation will just be a list of messages
 class message:
 	'''container for data that will be used by algorithm to label emotion'''
 	def __init__(self, content, eTag, userId):
-		self._content = content
+		self._content = content.lower()
+		for c in string.punctuation:
+			self._content.replace(c, "")
 		self._eTag = eTag.lower()
 		assert(userId in (1,2))
 		self._userId = userId #possible values are 1 or 2
